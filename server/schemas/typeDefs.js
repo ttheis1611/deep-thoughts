8f407c949734c57dc4bac6301dedc61de2fb4c4e
Terry Theis <tltheis@prodigy.net>
2021-07-05 18:51:07 -0500

    login

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,36 +3,41 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-type Thought {
-    _id: ID
-    thoughtText: String
-    createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
-}
-
-type Reaction {
-    _id: ID
-    reactionBody: String
-    createdAt: String
-    username: String    
-}
-
-type User {
+  type User {
     _id: ID
     username: String
     email: String
     friendCount: Int
     thoughts: [Thought]
     friends: [User]
-}
+  }
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
 
   type Query {
-      users: [User]
-      user(username: String!): User
-      thoughts(username: String): [Thought]
-      thought(_id: ID!): Thought
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): User
+    addUser(username: String!, email: String!, password: String!): User
   }
 `;
 
